1241256a025f95a5c6617dcf9429ab18176524c8
Tom C Watson <tom.watson@dataforaction.org.uk>
2024-03-18 20:54:22 +0000

    Revert "Update supabaseClient.js"
    This reverts commit aade89b1f8a78b4a8a237a348db11bfaaaf85ac0.

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+const supabaseUrl = 'https://xxinluhuqvufoeputzvb.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4aW5sdWh1cXZ1Zm9lcHV0enZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2MTAyNDUsImV4cCI6MjAyNjE4NjI0NX0.fKWvLVlrYb2J7Z3eCuKXNyv0tE0ZbzQMG6Pq2HTujUo';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
