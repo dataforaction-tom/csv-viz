aade89b1f8a78b4a8a237a348db11bfaaaf85ac0
Tom C Watson <tom.watson@dataforaction.org.uk>
2024-03-18 20:31:54 +0000

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://xxinluhuqvufoeputzvb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4aW5sdWh1cXZ1Zm9lcHV0enZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2MTAyNDUsImV4cCI6MjAyNjE4NjI0NX0.fKWvLVlrYb2J7Z3eCuKXNyv0tE0ZbzQMG6Pq2HTujUo';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
