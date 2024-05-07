9a1206901c022db6971d4dea01e0bc96f32aa3c1
Tom C Watson <tom.watson@dataforaction.org.uk>
2024-05-07 21:47:54 +0100

    Update Journey.js

--- src/Journey.js
+++ src/Journey.js
@@ -5,7 +5,7 @@ const Journey= () => {
  return(
   <div>
 <iframe src="https://v2-embednotion.com/b53e4b066c2440f6b91f1ad0f334fc8c" width="100%"
-        height="574"  ></iframe>
+        height="600"  ></iframe>
 </div>  
  )
 }
