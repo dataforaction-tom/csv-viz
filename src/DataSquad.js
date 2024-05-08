da4ab5516a46c3303177484481258df4c0f67ca7
Tom C Watson <tom.watson@dataforaction.org.uk>
2024-05-08 21:51:26 +0100

    Update DataSquad.js

--- src/DataSquad.js
+++ src/DataSquad.js
@@ -3,18 +3,7 @@ import React from 'react';
 const DataSquad = () => {
   return (
     <div style={{ width: '100vw', height: '100vh', padding: 0, margin: 0 }}>
-      <iframe
-        src="https://v2-embednotion.com/b53e4b066c2440f6b91f1ad0f334fc8c"
-        style={{
-          width: '100%',
-          height: '100%',
-          border: '2px solid #ccc',
-          borderRadius: '10px',
-          padding: 'none',
-          overflow: 'hidden'
-        }}
-        title="Journey"
-      ></iframe>
+      <h1>Data Squad stuff to go here</h1>
     </div>
   );
 };
