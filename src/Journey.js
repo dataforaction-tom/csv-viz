a8773d85048dcbac60892efee010c5753841f63f
Tom C Watson <tom.watson@dataforaction.org.uk>
2024-05-20 11:13:34 +0100

    Update Journey.js

--- src/Journey.js
+++ src/Journey.js
@@ -4,17 +4,9 @@ const Journey = () => {
   return (
     <div style={{ width: '100vw', height: '100vh', padding: 0, margin: 0 }}>
       <iframe
-        src="https://v2-embednotion.com/b53e4b066c2440f6b91f1ad0f334fc8c"
-        style={{
-          width: '100%',
-          height: '100%',
-          border: '2px solid #ccc',
-          borderRadius: '10px',
-          padding: 'none',
-          overflow: 'hidden'
-        }}
-        title="Journey"
-      ></iframe>
+  src="https://insight-infrastructure.super.site/"
+  className='w-full h-full'
+></iframe>
     </div>
   );
 };
